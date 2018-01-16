ed977d02f29e5a6ca64ee25cb9aebe7dab296ef7
spencermason <35467465+spencermason@users.noreply.github.com>
2018-01-16 18:36:13 -0500

    Update

---
+++ Assets/ResetOval.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOval : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 move = Vector3.zero;
+            move.x = 0;
+            move.y = 5;
+            move.z = 0;
+            gameObject.transform.position = move;
+        }
+    }
+
+
+}
